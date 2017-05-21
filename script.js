7b133c7f461c122ffd5633b2a5e80fc441961182
Ari Frankel <ari.l.frankel@gmail.com>
2017-05-21 18:54:55 -0500

    first commit. 5/21 site revision

---
+++ script.js
@@ -0,0 +1,34 @@
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+  function checkSlide(e){
+    sliderImages.forEach(sliderImage => {
+      const windowBottom = window.scrollY + window.innerHeight
+      const slideInAt = windowBottom + sliderImage.height / 2
+      const imageBottom = sliderImage.offsetTop + sliderImage.height
+      
+      const isHalfShown = slideInAt > sliderImage.offsetTop
+      const isNotScrolledPast = window.scrollY < imageBottom
+      
+      if (isHalfShown && isNotScrolledPast) {
+        sliderImage.classList.add('active')
+      } else {
+        sliderImage.classList.remove('active')
+      }
+    })
+  }
+
+  const sliderImages = document.querySelectorAll('.slide-in')
+  window.addEventListener('scroll', debounce(checkSlide))
